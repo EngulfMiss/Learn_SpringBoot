c7344471efdeb67f130e3004d57c8ef127973b5e
EngulfMiss <61497283+EngulfMiss@users.noreply.github.com>
2021-07-09 06:57:31 +0800

    kuang

---
+++ springboot-01-helloworld/src/main/java/com/engulf/helloworld/HelloworldApplication.java
@@ -0,0 +1,16 @@
+package com.engulf.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//程序的主入口
+//@SpringBootApplication：标注这个类是一个springboot的应用
+@SpringBootApplication
+public class HelloworldApplication {
+
+    public static void main(String[] args) {
+        //将springboot应用启动
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+}
---
+++ springboot-01-helloworld/src/test/java/com/engulf/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engulf.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
